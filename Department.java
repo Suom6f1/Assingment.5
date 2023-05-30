836bddc324c90b1937da54e1fef67cc40b854f64
Suhail Sheriff <108292990+Suom6f1@users.noreply.github.com>
2023-05-31 02:29:58 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,20 @@
+package org.sytem;
+
+
+
+	public class Department extends Collage {
+		
+		public void departmentName() {
+			System.out.println("CSE");
+		}
+		
+		
+		
+
+		public static void main(String[] args) {
+			Department call = new Department();
+			call.departmentName();
+
+		}
+
+	}
